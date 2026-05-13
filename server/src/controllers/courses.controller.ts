3c22ededf153a1148d376868c8799b19dd851936
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-05-14 07:13:26 +0800

    Fetching of courses and schools

--- server/src/controllers/courses.controller.ts
+++ server/src/controllers/courses.controller.ts
@@ -5,13 +5,13 @@ import { CourseService } from '../services/courses.service';
 export class CoursesController {
   constructor(private readonly courseService: CourseService) {}
 
-  @Get()
+  @Get('fetch-all')
   async getAllCourses(@Query('count') count: string) {
     const limit = parseInt(count, 10) || 10;
     return await this.courseService.getAllCourses(limit);
   }
 
-  @Post()
+  @Post('insert')
   async insertCourse(@Body('course') course: string) {
     return await this.courseService.insertCourse(course);
   }
