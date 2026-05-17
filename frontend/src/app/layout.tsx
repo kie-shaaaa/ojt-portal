ea250895cdbaa317b6a8694c793e363377231304
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-05-18 06:59:47 +0800

    Changes for the packages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col overflow-x-hidden vc-init">
+      <body
+        suppressHydrationWarning
+        className="min-h-full flex flex-col overflow-x-hidden vc-init"
+      >
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
