f365b3fb592f681e4fbb1460d22881ac86f77b47
Adrian-Fab <fabrosadrian0@gmail.com>
2026-05-21 09:19:11 +0800

    feat: added toast error messages for track application

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         className="min-h-full flex flex-col overflow-x-hidden vc-init"
       >
         <AuthProvider>{children}</AuthProvider>
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
