d61b2aadf52d94257d90044a5547b9702d657494
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-06-02 15:31:51 +0800

    fix: fix lint

--- backend/test/submit-application.e2e-spec.ts
+++ backend/test/submit-application.e2e-spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import {
